9c6a4e80ad10e935fc0ac9ca333a4d84a5dabde9
David Jimenez <david.jimenez19@gmail.com>
2015-08-17 12:13:08 +0200

    Defined default values for the AR model group event

--- app/models/group_event.rb
+++ app/models/group_event.rb
@@ -1,2 +1,11 @@
 class GroupEvent < ActiveRecord::Base
+
+  before_save :default_values
+
+  def default_values
+    self.deleted ||= 0
+    self.published ||= 0
+    self.duration ||= 0
+  end
+
 end
