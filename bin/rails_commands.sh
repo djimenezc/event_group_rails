4843f8a77374ca770330651b4255889b43069174
David Jimenez <david.jimenez19@gmail.com>
2015-09-01 11:03:52 +0200

    update sh file with the bash line

--- bin/rails_commands.sh
+++ bin/rails_commands.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 rails generate scaffold_controller group_events
 rails generate model GroupEvent name:string location:string description:text start_date:date duration:integer delete:boolean published:boolean
 rake db:create
